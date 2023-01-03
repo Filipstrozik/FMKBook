4760d35592fb3fd2815a8a9aa6b9aa63410f86fd
Filipstrozik <filipshelby@gmail.com>
2023-01-03 15:26:43 +0100

    add: entities from DB; started Agent service

--- springboot-backend/src/main/java/com/fmkbook/springbootbackend/repositories/AgentRepository.java
+++ springboot-backend/src/main/java/com/fmkbook/springbootbackend/repositories/AgentRepository.java
@@ -3,6 +3,10 @@ package com.fmkbook.springbootbackend.repositories;
 import com.fmkbook.springbootbackend.models.Agent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AgentRepository extends JpaRepository<Agent, Integer> {
+import java.util.Optional;
 
+public interface AgentRepository extends JpaRepository<Agent, Integer> {
+    void deleteAgentById(Integer id);
+
+    Optional<Agent> findAgentById(Integer id);
 }
