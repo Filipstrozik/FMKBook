57dae61598b4bacff230ce828fb2509ead90d19b
Filipstrozik <filipshelby@gmail.com>
2023-01-07 17:24:55 +0100

    add: typabonamentu-list

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BuyAbonamentListComponent } from './buy-abonament-list/buy-abonament-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BuyAbonamentListComponent
   ],
   imports: [
     BrowserModule
