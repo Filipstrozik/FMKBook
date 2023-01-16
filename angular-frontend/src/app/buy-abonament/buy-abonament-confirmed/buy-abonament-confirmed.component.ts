272f0377850e5d1a1250b486ca3bdf9374db29f5
Filipstrozik <filipshelby@gmail.com>
2023-01-16 16:36:09 +0100

    add:

--- angular-frontend/src/app/buy-abonament/buy-abonament-confirmed/buy-abonament-confirmed.component.ts
+++ angular-frontend/src/app/buy-abonament/buy-abonament-confirmed/buy-abonament-confirmed.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
+import {AbonamentService} from "../../services/abonament/abonament.service";
 
 @Component({
   selector: 'app-buy-abonament-confirmed',
@@ -8,10 +9,23 @@ import {ActivatedRoute, Router} from "@angular/router";
 })
 export class BuyAbonamentConfirmedComponent implements OnInit {
 
-  constructor(private router: Router, private route: ActivatedRoute) { }
+  private idUser: number;
+  private idTypAbonamentu: number;
+  constructor(private router: Router,
+              private route: ActivatedRoute,
+              private abonamentService: AbonamentService) {
+    console.log('user')
+    console.log(this.route.snapshot.paramMap.get('idUser'));
+    this.idUser = parseInt(<string>this.route.snapshot.paramMap.get('idUser'));
+    console.log('typabonamentu')
+    console.log(this.route.snapshot.paramMap.get('idTypAbonamentu'));
+    this.idTypAbonamentu = parseInt(<string>this.route.snapshot.paramMap.get('idTypAbonamentu'));
+  }
 
   ngOnInit(): void {
+    this.abonamentService.addNewAbonament(this.idUser, this.idTypAbonamentu).subscribe();
   }
+
 
 
   confirm() {
